9fc6df058c822338122cb307a3b4c42c7ec69dfa
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-01-29 20:02:27 +0500

    products

--- app/src/scripts/index.ts
+++ app/src/scripts/index.ts
@@ -3,15 +3,24 @@ const btn = document.querySelector('.header-bot__search-btn_2');
 btn.addEventListener('click', () => {
     search.classList.add('active');
 })
+
 window.addEventListener('click', (e) => {
     if ( !e.composedPath().includes(btn) && !e.composedPath().includes(search) ){
         search.classList.remove('active');
     }
 })
+
 var swiper = new Swiper(".slider__content     ", {
     spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-  });
+});
+
+const categories = document.querySelectorAll('.categories__item');
+categories.forEach(el => {
+    el.addEventListener('click', (e) => {
+        el.classList.toggle('active');
+    })
+});
